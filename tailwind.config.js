e2f679b7aa91970373f8837b257f2d4477de351b
SokvisalMong <smong@paragoniu.edu.kh>
2022-10-21 23:22:34 +0700

    finished setting up tailwind css and bootstrap

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resoucres/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
