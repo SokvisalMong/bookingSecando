af97b1143c6d860f2c027c90122f708e92842b52
SokvisalMong <smong@paragoniu.edu.kh>
2022-10-23 23:15:59 +0700

    added data to faqs migration

--- database/migrations/2022_10_23_115025_create_faqs_table.php
+++ database/migrations/2022_10_23_115025_create_faqs_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('faqs', function (Blueprint $table) {
             $table->id();
+
+            $table->text('question');
+            $table->text('answer')->nullable();
+
             $table->timestamps();
         });
     }
